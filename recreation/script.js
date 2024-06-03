0954cb99bff30e74602b4cd1613ed1b0408c1d81
Zayidx <faridindrawan0236@gmail.com>
2024-06-03 12:00:19 +0700

    DONE

--- recreation/script.js
+++ recreation/script.js
@@ -1,40 +1,30 @@
-// Navbar
-  let menu = document.querySelector('#menu-icon');
-  let navbar = document.querySelector('.navbar');
+$(function() {
 
-  menu.onclick = () => {
-      navbar.classList.toggle('active');
-  }
-
-  window.onscroll = () => {
-      navbar.classList.remove('active');
-  }
-  // Dark Mode
-  let darkmode = document.querySelector('#darkmode');
-
-  darkmode.onclick = () => {
-      if(darkmode.classList.contains('bx-moon')){
-          darkmode.classList.replace('bx-moon','bx-sun');
-          document.body.classList.add('active');
-      }else{
-          darkmode.classList.replace('bx-sun','bx-moon');
-          document.body.classList.remove('active');
-      }
-  }
-
-  // Scroll Reveal
-  const sr = ScrollReveal ({
-      origin: 'top',
-      distance: '40px',
-      duration: 2000,
-      reset: true
-  });
-
-
-  sr.reveal(`.home-text, .home-img,
-              .about-img, .about-text,
-              .box, .s-box,
-              .btn, .connect-text,
-              .contact-box`, {
-      interval: 200
-  })
+    // Menu Responsive
+    $(".menu-responsive").toggle();
+  
+    $(".menu-bars").on("click", function() {
+        $(".menu-responsive").toggle("slow");
+    });
+  
+  
+    // Scroll click on menu
+    $(".scroll").on("click", function() {
+        $(this).each(function() {
+            const sectionTop = $(this.hash).offset().top;
+            $("html, body").animate({
+                scrollTop: sectionTop
+            }, 1500);
+        });
+    });
+  
+  
+    // Progress Bar Animate
+    window.sr = ScrollReveal();
+    sr.reveal(".progress-bar", {
+       origin: "left",
+       duration: 2000,
+       distance: "100%"
+    });
+  
+})
