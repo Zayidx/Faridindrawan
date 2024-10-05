9117c32c30494c1c20e83226be6a6129db2f7ca2
Farid Indrawan <128061930+Zayidx@users.noreply.github.com>
2024-10-05 15:30:21 +0700

    completemobile

--- assests/main.js
+++ assests/main.js
@@ -85,34 +85,8 @@ const countDownClock = (endDate) => {
   Format: '2024-12-31T23:59:59' for countdown to Dec 31, 2024 at 11:59:59 PM.
   Make sure HTML elements with class 'days', 'hours', 'minutes', 'seconds' exist.
 */
-countDownClock('2024-09-17T18:00:00');
+countDownClock('2028-11-13T00:00:00');
 
-
-
-  // Carousel
-  let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-
-  slides[slideIndex-1].style.display = "block";  
-
-}
 
 
 
